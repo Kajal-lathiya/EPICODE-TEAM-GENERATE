5a54a1ccf4325beb9711b9a53e248fb75e91b9ca
Kajal <kajalbodarya@gmail.com>
2022-10-28 21:06:41 +0530

    add members into teams

--- script.js
+++ script.js
@@ -5,37 +5,55 @@ const teamInputNode = document.querySelector('.team-input')
 const UserListNode = document.getElementById('user-list');
 const teamContainer = document.querySelector('.team-container');
 let userList = [];
+let teamCount = teamInputNode.value;
 AddButton.addEventListener('click', () => {
     if (userInputNode.value !== "") {
-        userList.push({ name: userInputNode.value });
-        showUserList();
+        userList.push(userInputNode.value);
+        UserListNode.style.border = '1px solid black';
+        let p = document.createElement('p');
+        p.innerText = userInputNode.value;
+        UserListNode.appendChild(p);
     } else {
         alert('Enter User name')
     }
     userInputNode.value = "";
 })
 
-const showUserList = () => {
-    UserListNode.style.border = '1px solid black';
-    userList.map((obj, i) => {
-        let p = document.createElement('p');
-        p.innerText = obj.name;
-        UserListNode.appendChild(p);
-    })
-}
-
 AddTEAMButton.addEventListener('click', () => {
-    console.log('call-->', teamInputNode.value);
-    let number = teamInputNode.value;
-    for (let i = 0; i < number; i++) {
-        let div = document.createElement('div');
+    let newarray = (shuffleArray(userList));
+    let teamArray = splitTeams(newarray, teamInputNode.value);
+    let div;
+    teamArray.map((obj, i) => {
+        div = document.createElement('div');
         div.className = 'team-list';
         let h3 = document.createElement('h3');
-        h3.innerText = i + 1;
+        h3.innerText = `TEAM ${i + 1}`;
         div.appendChild(h3);
         teamContainer.appendChild(div)
-    }
+        obj.map((d) => {
+            let p = document.createElement('p');
+            p.innerText = d;
+            div.appendChild(p);
+        })
+    })
 })
-
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * i);
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
+}
+function splitTeams(names, teams_count) {
+    let teams = [];
+    while (teams_count > 0) {
+        teams.push(names.splice(0, Math.floor(names.length / teams_count)))
+        teams_count--;
+    }
+    return teams
+}
 window.onload = () => {
+
 }
